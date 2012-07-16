e7ee430f1c7dcb04bea4597014cbbe006a4e4cdd
Nick HS <nickhs@umich.edu>
2012-07-16 15:56:30 -0700

    Changed message

--- foo.py
+++ foo.py
@@ -12,7 +12,7 @@ def main():
 @app.route('/batsignal')
 def batsignal():
     topic = "arn:aws:sns:us-east-1:209247679858:Test"
-    sns.publish(topic, "Something important!")
+    sns.publish(topic, "There are cookies in the vincinity")
     return 'okay'
 
 if __name__ == '__main__':
